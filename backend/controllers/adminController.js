1c1d6f5e131685356c87fcbcbc2ba1dc4436c30c
Alex <angrybird3868@gmail.com>
2025-11-26 22:21:48 -0500

    Implement secrets for SendGrid and use it for user regristration

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,5 @@
 const pool = require('../config/database');
+const { sendEmail } = require("../config/sendGrid");
 
 const createUser = async (req, res) => {
   try {
@@ -9,18 +10,17 @@ const createUser = async (req, res) => {
       return res.status(422).json({ message: "Missing required fields." });
     }
 
-    // Insert into users table
+    // Insert into Users table
     const insertUserQuery = `
       INSERT INTO Users (Type, Email, PasswordHash)
       VALUES ($1, $2, '')
       RETURNING UserID;
     `;
-
     const userResult = await pool.query(insertUserQuery, [role, email]);
     const newUserId = userResult.rows[0].userid;
 
-    //  Insert into role specific table
-    if (role.toLowerCase() === 'student') {
+    // Insert into the correct role-specific table
+    if (role.toLowerCase() === "student") {
       const insertStudentQuery = `
         INSERT INTO Student (UserID, StudentName, SchoolID, SchoolName)
         VALUES ($1, $2, $3, $4);
@@ -31,8 +31,7 @@ const createUser = async (req, res) => {
         schoolid,
         schoolname
       ]);
-    } 
-    else if (role.toLowerCase() === 'guidancecounsellor') {
+    } else if (role.toLowerCase() === "guidancecounsellor") {
       const insertGCQuery = `
         INSERT INTO GuidanceCounsellor (UserID, CounsellorName, SchoolID, SchoolName)
         VALUES ($1, $2, $3, $4);
@@ -45,9 +44,33 @@ const createUser = async (req, res) => {
       ]);
     }
 
+    // Send registration email
+    await sendEmail({
+      to: email,
+      subject: "Welcome to VolunCloud!",
+      text: `Hello ${name}, you're school has added you to VolunCloud: the student volunteer hour management system. 
+Visit the link to finish setting up your login.`,
+      html: `
+        <p>Hello ${name},</p>
+        <p>You have been added to the <strong>Student Volunteer Hours Manager</strong> system.</p>
+        <p>Please click the button below to finish creating your account:</p>
+
+        <p>
+          <a href="http://178.128.232.57/register"
+             style="padding: 12px 20px; background: #4CAF50; color: white; text-decoration: none; 
+                    border-radius: 6px; font-size: 16px;">
+            Set Up Your Account
+          </a>
+        </p>
+
+        <br/>
+        <p>— Student Volunteer Hours Manager Team</p>
+      `
+    });
+
     return res.status(200).json({
       success: true,
-      message: "User created successfully",
+      message: "User created and email sent successfully",
       userId: newUserId
     });
 
@@ -57,4 +80,4 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = {createUser};
+module.exports = { createUser };
